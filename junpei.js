cdc65ce71074b0a73eac2a54eb0d195c4fe9230f
junkun0129 <junkun0129@mail.com>
2022-05-17 15:54:42 -0700

    first commit

---
+++ junpei.js
@@ -0,0 +1,20 @@
+console.log("iwatani");
+console.log("iwatani");
+ 
+sum 
+\\\|||||||
+
+function sum(){
+    13+2
+}
+var age = 20
+
+if(age <18){
+
+    console.log("lskdfjslkdfjf")
+}else{
+    console.log("slkdjfjsldkf")
+}
+
+
+
